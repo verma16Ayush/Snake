ddc73344936ba66c079b4cdc47d38a34e725890a
Ayush <ayush.bravo16@gmail.com>
2020-06-22 13:03:50 +0530

    fixed a bug where fruit was spawned outof the game bounds. fixed a bug where cosole text was not colored as wanted. added a main menu. added a game over screen.

--- snake.c
+++ snake.c
@@ -1,3 +1,12 @@
+// snake game
+// run this file to play
+
+
+// in case the entire play area is not visible 
+// do not maximize the console window instead drag borders to resize it.
+// maximizing the windo overrides the hide cursor method and makes the game look messy
+
+
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
@@ -9,13 +18,20 @@
 int width = 40; 
 int height = 20;
 
+//store coordinates of snake compoents or whatev
 int snakeX[100], snakeY[100];
 
 //size of snake
-int snakeSize = 0;
+int snakeSize = 1;
+
+//store the delay netween each frame and hence controls the game speed
+int delayBwFrames = 250;
 
 // variables to store x and y poosition of snake head and fruit
-int headPosX, headPosY, fruitPosX, fruitPosY, score;
+int headPosX, headPosY, fruitPosX, fruitPosY;
+
+//store the score and highest score for that session
+int score = 0, highScore = 0;
 
 //if the game has ended
 bool hasEnded = false;
@@ -23,16 +39,26 @@ bool hasEnded = false;
 //store the move direction of the snake. wasd keymap. x to quit.
 char moveDir;
 
+void MainMenu(); // self explanatory
 void Draw(); //draw the play board.
 void Setup(); // setup everyhting for a fresh start.
 void Input(); // get player input if any.
 void Move(); // make the snake do snaky stuff.
+void GameLoop();// the main game loop
 void HideCursor(bool); // hide the console cursor. makes stuff look bad.
 void SpawnSnake(); // spawn a snaky bad boi.
 void SpawnFood(); // spawn delicious food bois.
 void GameplayCode(); // handles the main gameplay logic of win / lose / reward checking.
 void Delay(int); // Delay code execution.
 void Bend(); // handles the bending logic of snake, seemed logical to keep it in another method.
+void SetConsoleColor(); // set the console text color to green
+void GameOver(); // handles game over process
+
+
+void SetConsoleColor()
+{
+    printf("\033[1;34m");
+}
 
 void HideCursor(bool hideOrShow)
 {
@@ -53,13 +79,47 @@ void Delay(int milliSeconds)
         {}; 
 } 
 
+void MainMenu()
+{
+    printf("WELCOME TO THE SNAKE GAME!\n\n");
+    printf("Y/y: Put me in coach!\n");
+    printf("Anything else: I'm too afraid of console snakes\n");
+    printf("$> ");
+    char yes;
+    scanf("%c", &yes);
+    switch (yes)
+    {
+    case 'y':
+    case 'Y':
+        break;
+    default:
+        exit(0);
+        break;
+    }
+}
+
 void Setup()
 {
     //seed the PRNG
     srand(time(0));
 
+    //reset hasEnded flag
+    hasEnded = false;
+
+    //set console color
+    SetConsoleColor();
+
+    //hide cursor
+    HideCursor(false);
+
     //reset the snake size
     snakeSize = 1;
+
+    //reset the game speed
+    delayBwFrames = 250;
+
+    //reset score
+    score = 0;
     
     //spawn a brand new snake
     SpawnSnake();
@@ -79,10 +139,10 @@ void SpawnFood()
     fruitPosX = rand() % width - 1;
     fruitPosY = rand() % height - 1;
 
-    if(fruitPosX == 0)
+    if(fruitPosX <= 0)
         fruitPosX = 1;
     
-    if(fruitPosY == 0)
+    if(fruitPosY <= 0)
         fruitPosY = 1;
 }
 
@@ -96,25 +156,30 @@ void Input()
 
 void Move()
 {
-    TestMethod();
+    Bend();
 
     switch (moveDir)
     {
     case 'w':
         headPosY--;
         break;
+
     case 's':
         headPosY++;
         break;
+
     case 'a':
         headPosX--;
         break;
+
     case 'd':
         headPosX++;
         break;
+
     case 'x':
         hasEnded = true;
         break;
+        
     default:
         break;
     }
@@ -165,8 +230,7 @@ void Draw()
         }
         printf("\n");
     }
-    printf("\nScore: %d       size: %d", score, snakeSize);
-    
+    printf("\nScore: %d", score);
 }
 
 void Bend()
@@ -195,31 +259,86 @@ void Bend()
 
 void GameplayCode()
 {
+    //when snake eats the food
     if(headPosX == fruitPosX && headPosY == fruitPosY)
     {
         score += 10;
         SpawnFood();
         snakeSize += 1;
+        delayBwFrames -= 10;
+
+        if(score > highScore)
+        {
+            highScore = score;
+        }
     }
 
+    //when snake goes out of game bounds
     else if(headPosX >= width - 1 || headPosY >= height - 1 || headPosX <= 0 || headPosY <= 0)
     {
         hasEnded = true;
     }
+
+    //when snake touches its own body
+    //TODO: write code
+
+    for (int i = 1; i < snakeSize; i++)
+    {
+        if(headPosX == snakeX[i] && headPosY == snakeY[i])
+        {
+            hasEnded = 1;
+        }
+    }
+    
 }
 
-int main()
+void GameOver()
 {
-    HideCursor(false);
-    Setup();
+    system("cls");
+    printf("YOUR GAME HAS ENDED!\n");
+    printf("YOUR SCORE: %d\n", score);
+
+    if(highScore == score)
+    {
+        printf("NEW HIGH SCORE SET!!\n");
+    }
+    printf("Do you want to play again\nenter y for yes, any other key to exit:\n");
+    char ch = getch();
+    if(ch == 'y' || ch == 'Y')
+   {
+        Setup();
+        GameLoop();
+   }
+    else
+        exit(0);
+
+}
+
+void GameLoop()
+{
     while(!hasEnded)
     {
         Draw();
         Input();
         GameplayCode();
         Move();
-        Delay(10);
+        if(delayBwFrames >= 0)
+        {
+            Delay(delayBwFrames);
+        }
     }
-    scanf("%d");
+
+     if(hasEnded)
+        {
+            GameOver();
+        }
+}
+
+int main()
+{
+    system("cls");
+    Setup();
+    MainMenu();
+    GameLoop();
     return 0;
 }
